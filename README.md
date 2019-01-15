aba265146ce0f7d1d64bbb49b48047d02f0bdbea
Thomas Copt-Bignon <lowx.add@gmail.com>
2019-01-15 11:21:18 +0100

    Initial commit (part 2)

---
+++ README.md
@@ -0,0 +1,10 @@
+# The Valley Server Bot
+>A simple Discord Bot made for the server of a friend.
+
+# ⚠️
+The bot is still in construction !
+
+## Modules :
+I used 2 modules for this bot :
+- [Discord.js](https://discord.js.org/)
+- [Node.js](https://nodejs.org/)
---
+++ bot.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const { Client, RichEmbed } = require('discord.js');
+
+client.on('ready', () => {
+  console.log("Bot deployed.");
+});
+
+client.on('message', message => {
+  var prefix = ".";
+
+  // Make the bot reply to ".hi"
+  if (message.content === prefix + 'hi') {
+    message.channel.send("Hi, " + message.author.toString() + ". :smile:")
+    console.log(`Replied hi to ${message.author.username} in ${message.guild.name}`);
+  }
+
+  // Make the bot say whatever you want by typing .say "yout message"
+  if(message.content.substr(0, 8) === prefix + "say") {
+    var messageContent = message.content.substr(5, message.content.length);
+    message.delete().catch(O_o => {});
+    message.channel.send(messageContent);
+  }
+
+});
+
+client.login(process.env.BOT_TOKEN);
